848a7af3a37b23e6bf2c9e7e7a559e44537ad058
Maja <dobry.email@interia.pl>
2025-11-16 12:01:37 +0100

    Update fastfetch_setup.sh

--- fastfetch_setup.sh
+++ fastfetch_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 mkdir -p ~/.config/fastfetch
-cp -r config.jsonc ~/.config/fastfetch
+cp -r config.jsonc kafkastare.png ~/.config/fastfetch
 
 echo "Fastfetch config updated successfully! UwU"
 
 cd --
 rmdir fastfetch-theme-mae
+fastfetch
